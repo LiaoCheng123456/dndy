3a099e436d081da5e6a6ae2e25e6aa1e3e552ac6
lc <1337547531@qq.com>
2021-01-27 10:44:10 +0800

    更新

--- src/main/java/com/dndy/cotroller/BaseController.java
+++ src/main/java/com/dndy/cotroller/BaseController.java
@@ -14,5 +14,5 @@ public class BaseController implements Serializable {
 
 	private static final long serialVersionUID = 6357869213649815390L;
 
-	protected JSONObject json = new JSONObject();
+	protected static JSONObject json = new JSONObject();
 }
